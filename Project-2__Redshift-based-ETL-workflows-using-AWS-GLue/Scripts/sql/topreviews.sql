74841d9a5645a35ffcd75742c30ae597c7566461
Prashant S <prashantsh0404@gmail.com>
2021-08-09 12:53:18 -0700

    commit

--- Project-2__Redshift-based-ETL-workflows-using-AWS-GLue/Scripts/sql/topreviews.sql
+++
@@ -1,3 +0,0 @@
-UNLOAD ('SELECT marketplace, product_category, product_title, review_id, helpful_votes, AVG(star_rating) as average_stars FROM reviews GROUP BY marketplace, product_category, product_title, review_id, helpful_votes ORDER BY helpful_votes DESC, average_stars DESC')
-TO 's3://etl-rs-g-databucket-1qgcxeb2m2ko6/testunload/'
-iam_role 'arn:aws:iam::457403320578:role/ETL-RS-G-RedshiftRole-T3CUR4SC6852';
